67cb7cf4cb87fca64c5a944a0577380bc24dcfde
Chris <cjbouwens@gmail.com>
2019-06-12 15:57:15 +0100

    Update DT6606_renato_NetpanMHC.R

--- DT6606_renato_NetpanMHC.R
+++ DT6606_renato_NetpanMHC.R
@@ -1,15 +1,55 @@
-if (!requireNamespace("BiocManager", quietly = TRUE))
-  install.packages("BiocManager")
-
-BiocManager::install("UniProt.ws")
-BiocManager::install("biomaRt")
-install.packages("data.table")
-install.packages("styler")
-install.packages("xml2")
-
 library(UniProt.ws)
 library(data.table)
 library(biomaRt)
+
+
+rsubstr <- function(x,n) {
+# substring only works from left to right.
+# this function grabs n characters from the end of the string.
+
+  substring(x, nchar(x) - n + 1)
+}
+
+
+Ncharextract <- function(x, n, loc, from, replace = TRUE, mut = "X") {
+  # extracts +n and -n characters surrouding a location in a vector.
+  #
+  # Args:
+  #   x: A vector where the characters will be extracted from
+  #   n: The amount of characters to be extracted from the vect,
+  #      should be a positive integer.
+  #   loc: Numeric location in the vector from where
+  #      surrounding characters are extracted.
+  #   from : Original amino acid in the mutated location. e.g. A in A12K.
+  #   replace: If TRUE, replaces the character at position loc
+  #      with the character supplied in mut. Default is TRUE
+  #   mut: character that replaces original character in location loc.
+  #
+  # Returns:
+  #   A vector of 2n+1 length extracted from vector x.
+  mer <- c()
+  if (is.na(x)) {
+    stop("x is not a character or numeric vector, or x is not supplied.")
+  }else if (x == "Sequence unavailable") {
+    mer <- NA
+  }else if (0.5 * n > nchar(x)) {
+    mer <- "Sequence too short"
+  }else if (substring(x, first = loc, last = loc) == from) {
+     Rmer <- substring(x, first = loc - n, last = loc - 1)
+     Lmer <- substring(x, first = loc + 1, last = loc + n)
+
+  if (replace == TRUE) {
+    mid <- mut
+  }else{
+    mid <- substring(x, first = loc, last = loc)
+  }
+
+  mer <- paste(Rmer, mid, Lmer, sep = "")
+}else{
+  mer <- "Incorrect mutation location or sequence"
+}
+}
+
 
 # load in annotated whole exsome sequencing single nucleotide variation data.
 read.table("~/Documents/GitHub/gits/DT6606_variants_sub.tsv",
@@ -29,14 +69,6 @@ usedat <- dat[ -(1:30)]
 # gather nonsynonymous, exonic variations.
 nonsynonmut <- usedat[ExonicFunc.refGene == "nonsynonymous SNV"]
 
-
-
-rsubstr <- function(x,n) {
-# substring only works from left to right.
-# this function grabs n characters from the end of the string.
-
-  substring(x, nchar(x) - n + 1)
-}
 
 # multiple NCBI gene variants and corresponding CNV location
 # are located in the same column, we extract the last 50 digits and stringsplit
@@ -69,31 +101,6 @@ dfaasplit <- as.data.frame(aasplit,
 
 dfaasplit <- t(dfaasplit)
 
-# TO DO: add maxlength to give the maximum number of cycles per mutation.
-#        create while (sequence is NA AND k> max number of cycles, k +5 (to the new NM_####)) loop and run again.
-#lapply(aasplit, function(x) {
-#  unit1 <- unlist(x)[2] #hier neemt hij alleen de eerste van elke list.
-#  k=2
-#  sapply(unit1, function(i) {
-  #  match(i[2], )
-  #}
-#})
-
-
-#firstele <- lapply(aasplit, "[[", 1)
-
-#aalistlengths <- lapply(AAmutsplit, length)
-
-#AAmutsplit[[8]][2]
-#class(AAmutsplit)
-#str(AAmutsplit)
-#length(AAmutsplit)
-#AAmustsep <- sub(x = AAmutsmall,
-#  replacement = "",
-#  pattern = ".*,")sapply(AAmutsplit, function(x) {
-
-
-
 # grab the NCBI refseq reference, gene name, original amino acid,
 # location of the mutation and mutation.
 NM  <- unlist(dfaasplit[, 2], use.names = F)
@@ -107,6 +114,7 @@ to <- sapply(mut, function(x) {
   }
   , USE.NAMES = F)
 name <- unlist(dfaasplit[, 1], use.names = F)
+
 
 # select m. musculus uniprot datbase for AA sequence extraction.----
 mouseUp <- UniProt.ws(10090)
@@ -161,7 +169,10 @@ NMsub <- sub(x = resref[, 1],
 # build up final table
 resref_fin <- cbind(NMsub, resref, res2)
 
-NMmatch <- match(unlist(dfaasplit[,2 ], use.names = F),resref_fin[, 1])
+NMmatch <- match(unlist(dfaasplit[, 2],
+  use.names = F),
+  resref_fin[, 1])
+
 AAmat <- resref_fin[NMmatch, c(-3, -4)]
 AAdata <- cbind(name,
   NM,
@@ -171,24 +182,21 @@ AAdata <- cbind(name,
   mutpos,
   AAmat,
   stringsAsFactors = F)
-#end-----
+# end of uniprot query----
 
-# biomart approach
+# additional biomart query
 mart = useMart("ensembl", dataset="mmusculus_gene_ensembl")
-listAttributes(mart)
-searchAttributes(mart, pattern = "symbol")
-searchAttributes(mart, pattern = c("peptide"))
-searchFilters(mart, pattern = "mgi")
+
 annot <- getBMlist(attributes =
   "ensembl_gene_id",
   "refseq_mrna",
   "peptide",
   filters = "refseq_mrna",
   values  = NM,
-   mart=mart)
+  mart = mart)
 
-AAdata
-
+# because the UNIprot ID does no always net a sequence we go over the mutations
+# again but query the refseq ID and otherwise the protein name.
 for (i in seq_along(AAdata[,1])) {
   # If there is no sequence (NA), search biomaRt for the refeseq sequence.
   if (is.na(AAdata[i, 11])) {
@@ -206,6 +214,7 @@ for (i in seq_along(AAdata[,1])) {
           seqType = "peptide",
           mart = mart)
 
+          # if neither are available return Sequence unavailable
           if(!is.na(martseq_mgi[1, 1]) ||
           martseq_mgi[1, 1] != "Sequence unavailable"){
             AAdata[i, 11] <- unlist(martseq_mgi[1, 1], use.names = F)
@@ -216,72 +225,41 @@ for (i in seq_along(AAdata[,1])) {
     }
   }
 
-martseq_nm <- getSequence(id = unlist(AAdata[1, 2], use.names = F),
-  type = "refseq_mrna",
-  seqType = "peptide",
-  mart = mart,
-  verbose = FALSE)
-  str(martseq_nm)
-  is.na(martseq_nm[1,1])
-  is.null(unlist(martseq_nm[1,1]))
-
-# how many will return if I use simple gene names?
-martseq_mgi <- getSequence(id = name[1],
-  type = "mgi_symbol",
-  seqType = "peptide",
-  mart = mart)
-
-length(unique())
-length(unique(NM))#30 unique refseq codes
-length(unique(martseq_nm[, 2])) #24/30
-length(unique(martseq_mgi[, 2])) #17/30 met mgi_symbol
-length(unique(resref_fin[,7])) #19/30 met uniprotID.
-
-# using names doesn't really work because of transcript variants.
-# we should annotate into ENSGM and ENSTM. those should always net the correct
-# protein sequence.
-martseqmgi2 <- martseq_mgi[!martseq_mgi[, 1] == "Sequence unavailable", ]
-
-#lets continue with the resref_fin sequences.
-
-resref_fin
 
 
-Ncharextract <- function(x, n, loc, mut = "X") {
-  # extracts +n and -n characters surrouding a location in a vector.
-  #
-  # Args:
-  #   x: A vector where the characters will be extracted from
-  #   n: The amount of characters to be extracted from the vect,
-  #      should be a positive integer.
-  #   loc: Numeric location in the vector from where
-  #      surrounding characters are extracted.
-  #   replace: If TRUE, replaces the character at position loc
-  #      with the character supplied in mut. Default is TRUE
-  #   mut: character that replaces original character in location loc.
-  #
-  # Returns:
-  #   A vector of 2n+1 length extracted from vector x.
-  mer <- c()
-  if (is.na(x)) {
-    stop("x is not a character or numeric vector, or x is not supplied.")
-  }else{
-     Rmer <- substring(x, first = loc - n, last = loc - 1)
-     Lmer <- substring(x, first = loc + 1, last = loc + n)
-  }
 
-  if (replace == TRUE) {
-    mid <- mut
-  }else{
-    mid <- substring(x, first = loc, last = loc)
-  }
 
-  mer <- c(Rmer + mid + Lmer)
-}
+eightmers <- mapply(Ncharextract,
+  AAdata[, 11],
+  n = 8,
+  loc = AAdata[, 6],
+  from = AAdata[, 4],
+  mut = AAdata[, 5],
+  replace = TRUE
+  )
+
+ninemers <- mapply(Ncharextract,
+  AAdata[, 11],
+  n = 9,
+  loc = AAdata[, 6],
+  from = AAdata[, 4],
+  mut = AAdata[, 5],
+  replace = TRUE
+  )
+
+tenmers <- mapply(Ncharextract,
+  AAdata[, 11],
+  n = 10,
+  loc = AAdata[, 6],
+  from = AAdata[, 4],
+  mut = AAdata[, 5],
+  replace = TRUE
+  )
+
 
 
 UniproteinQC <- function (data, data2, seqcol, loc, expectedAA){
-  # qualit control function that checks if the the protein sequence was
+  # quality control function that checks if the the protein sequence was
   # successfully found and if the expected amino acid is in the right place.
   #
   # Args:
@@ -301,52 +279,6 @@ UniproteinQC <- function (data, data2, seqcol, loc, expectedAA){
     }
     # subsequently check if found protein sequences contain the expected AA.
 }
-
-
-#check for each protein sequence if the expected AA(from) is in position mut.
-mer8 <- c()
-mer9 <- c()
-mer10 <- c()
-
-for(i in seq_along(AAdata[,1])){
-
-  if (is.na(AAdata[i,"SEQUENCE"])){
-    mer8 <- c(mer8, "nosequence")
-    mer9 <- c(mer9, "nosequence")
-    mer10 <- c(mer10, "nosequence")
-  }else if (substring(AAdata[i,"SEQUENCE"], first = AAdata[i,"mutpos"], last = AAdata[i,"mutpos"]) == AAdata[i,"from"]){
-    #select -7 AA
-    rightmer8 <- c(substring(AAdata[i,"SEQUENCE"], first = AAdata[i,"mutpos"]-7, last = AAdata[i,"mutpos"]-1))
-    #select -8AA
-    rightmer9 <- c(substring(AAdata[i,"SEQUENCE"], first = AAdata[i,"mutpos"]-8, last = AAdata[i,"mutpos"]-1))
-    #select -9AA
-    rightmer10 <- c(substring(AAdata[i,"SEQUENCE"], first = AAdata[i,"mutpos"]-9, last = AAdata[i,"mutpos"]-1))
-    #select +7 AA
-    leftmer8 <- c(substring(AAdata[i,"SEQUENCE"], first = AAdata[i,"mutpos"]+1, last = AAdata[i,"mutpos"]+7))
-    #select +8AA
-    leftmer9 <- c(substring(AAdata[i,"SEQUENCE"], first = AAdata[i,"mutpos"]+1, last = AAdata[i,"mutpos"]+8))
-    #select +9AA
-    leftmer10 <- c(substring(AAdata[i,"SEQUENCE"], first = AAdata[i,"mutpos"]+1, last = AAdata[i,"mutpos"]+9))
-
-    #stitch them together
-    comb8 <- paste(rightmer8, AAdata[i,"to"], leftmer8, sep = "")
-    comb9 <- paste(rightmer9, AAdata[i,"to"], leftmer9, sep = "")
-    comb10 <- paste(rightmer10, AAdata[i,"to"], leftmer10, sep = "")
-
-    mer8 <- c(mer8, comb8)
-    mer9 <- c(mer9, comb9)
-    mer10 <- c(mer10, comb10)
-
-  }else{
-    mer8 <- c(mer8, "AA not found in sequence")
-    mer9 <- c(mer9, "AA not found in sequence")
-    mer10 <- c(mer10, "AA not found in sequence")
-  }
-
-}
-AAdata2 <- cbind(AAdata, mer8, mer9, mer10)
-
-#WIP find a way to deal with transcript variants when they don't have an uniprot sequence attached.
 
 
 write.csv(x=AAdata2,file = "~/Documents/MHC_neoantigen_select_WES/DT6606_renato_NetpanMHC.csv", row.names = F, col.names = T)
