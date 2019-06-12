9b253e9838a68fe12ffb6be0f4212636c2b1d636
Chris <cjbouwens@gmail.com>
2019-06-12 19:34:02 +0100

    Create MHCpipeline.sh

---
+++ MHCpipeline.sh
@@ -0,0 +1,3 @@
+#~!/bin/bash
+export PATH=~/Documents/netMHC-4.0:$PATH
+netMHC -f /home/christian/Documents/GitHub/gits/DT6606_renato_eightmer.txt -p -l 8 -xls -xlsfilename /home/christian/Documents/GitHub/gits/NETPAN_eightmer.xls 
