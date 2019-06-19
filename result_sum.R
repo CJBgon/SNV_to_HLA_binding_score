70448ac4339b393b0da9357d9cf22c6209191de5
Chris <cjbouwens@gmail.com>
2019-06-19 14:48:28 +0100

    Update result_sum.R

--- result_sum.R
+++ result_sum.R
@@ -2,6 +2,7 @@
 # 2019-06-18
 # visualise SNV - netMHCpan-4.0 results.
 
+library(RColorBrewer)
 library(ggplot2)
 library(data.table)
 
@@ -49,3 +50,24 @@ rowcount <- countdt[, lapply(.SD,sum), .SDcols = colnames(countdt[,2:6])]
 rowcount <- cbind(V1 = "peptidesum", rowcount)
 countsum <- rbind(countdt, rowcount)
 colnames(countsum) <- c("ID", "Db_High", "Kb_High", "Db_low", "Kb_low", "sum")
+countsum[,"ID"] <- factor(countsum$ID, levels = c("8mer", "9mer", "10mer", "peptidesum"))
+
+meltdt <- melt(countsum, id = "ID")
+display.brewer.all(colorblindFriendly = TRUE)
+my_palette = brewer.pal(5, "Set2")[c(3,5,4)]
+colour1 <- c("#67a9cf",
+  "#999999",
+  "#ffffff")
+
+names(my_palette) <- levels(countsum$ID)[1:3]
+colScale <- scale_colour_manual(name = "Var2",
+                      values = my_palette,
+                      aesthetics = "fill")
+
+
+barresults <- ggplot(data = meltdt[!ID == "peptidesum"], aes(x = variable, y = value, fill = ID)) +
+  geom_bar(stat = "identity")+
+  labs(fill = "Peptide", x = "MHC-I", y = "Count")+
+  scale_fill_brewer(palette = "Set2")
+
+tiff(file = )
